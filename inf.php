e786aedb409ce45abd1e5abd2e8031ed754d8351
Sam-Coelho <samcoelho137@gmail.com>
2025-09-24 00:03:16 +0100

    Add files via upload

---
+++ inf.php
@@ -0,0 +1,242 @@
+<?php
+$projetos = [
+    [
+        "titulo" => "Pintura a oleo Sapo",
+        "datainicio" => "2021-7-13",
+        "datafim" => "2021-07-14",
+        "relevancia" => "2",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["trabalho artesanal"],
+        "tipo" => ["ilustração"]
+    ],
+    [
+        "titulo" => "Jinx Ilustração",
+        "datainicio" => "2025-7-24",
+        "datafim" => "2025-07-27",
+        "relevancia" => "3",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Ilustração digital"],
+        "tipo" => ["ilustração"]
+    ],
+    [
+        "titulo" => "Fotografia, digitalização e exploração cromatica",
+        "datainicio" => "2023-2-20",
+        "datafim" => "2023-3-05",
+        "relevancia" => "3",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Fotografia","Ilustração digital"],
+        "tipo" => ["ilustração"]
+    ],
+    [
+        "titulo" => "Autoretrato",
+        "datainicio" => "2023-5-1",
+        "datafim" => "2023-5-16",
+        "relevancia" => "3",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Ilustração digital"],
+        "tipo" => ["ilustração"]
+    ],
+    [
+        "titulo" => "Narrativa visual",
+        "datainicio" => "2023-2-25",
+        "datafim" => "2023-3-5",
+        "relevancia" => "1",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Paginação","Ilustração digital"],
+        "tipo" => ["design"]
+    ],
+    [
+        "titulo" => "Identidade visual Museu",
+        "datainicio" => "2023-3-15",
+        "datafim" => "2023-4-09",
+        "relevancia" => "3",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Tipografia","Design de identidade visual","desenho vetorial"],
+        "tipo" => ["design"]
+    ],
+    [
+        "titulo" => "Pictogramas sobre criaturas miticas que representam paises",
+        "datainicio" => "2023-4-15",
+        "datafim" => "2023-5-31",
+        "relevancia" => "2",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Design de identidade visual","desenho vetorial"],
+        "tipo" => ["design"]
+    ],
+    [
+        "titulo" => "Paginação do Indice do livro 'The grand Design'",
+        "datainicio" => "2023-10-01",
+        "datafim" => "2023-10-13",
+        "relevancia" => "4",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Paginação","Tipografia"],
+        "tipo" => ["design"]
+    ],
+    [
+        "titulo" => "Paginação primeiro capitulo do livro 'The grand Design'",
+        "datainicio" => "2023-10-15",
+        "datafim" => "2023-11-17",
+        "relevancia" => "4",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Paginação","Tipografia"],
+        "tipo" => ["design"]
+    ],
+    [
+        "titulo" => "Paginação jornal",
+        "datainicio" => "2023-11-25",
+        "datafim" => "2023-12-17",
+        "relevancia" => "2",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Paginação","Tipografia"],
+        "tipo" => ["design"]
+    ],
+    [
+        "titulo" => "Construção 3d Ponte",
+        "datainicio" => "2024-2-10",
+        "datafim" => "2024-4-19",
+        "relevancia" => "3",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Programação (Java)","Modelação 3D"],
+        "tipo" => ["programação"]
+    ],
+    [
+        "titulo" => "Modelação e animação 3d barco",
+        "datainicio" => "2024-4-20",
+        "datafim" => "2024-6-13",
+        "relevancia" => "1",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Modelação 3D"],
+        "tipo" => []
+    ],
+    [
+        "titulo" => "Recensão sobre Arte contemporanea",
+        "datainicio" => "2025-1-12",
+        "datafim" => "2024-12-15",
+        "relevancia" => "5",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => [],
+        "tipo" => []
+    ],
+    [
+        "titulo" => "Cartazes sobre a obra 'Crimes exemplares'",
+        "datainicio" => "2024-10-10",
+        "datafim" => "2024-11-06",
+        "relevancia" => "3",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Ilustração digital","Tipografia","Paginação"],
+        "tipo" => ["design","ilustração"]
+    ],
+    [
+        "titulo" => "Desenvolvimento de um site de aluger de carros",
+        "datainicio" => "2024-10-10",
+        "datafim" => "2024-12-10",
+        "relevancia" => "4",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Web develop (HTML, CSS, JavaScript)","Tipografia","Base de dados"],
+        "tipo" => ["programação","design","webdesign"]
+    ],
+    [
+        "titulo" => "Animação Shapeless",
+        "datainicio" => "2025-3-10",
+        "datafim" => "2025-6-06",
+        "relevancia" => "5",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Ilustração digital","Tipografia","Animação frame a frame"],
+        "tipo" => ["Animação Frame a Frame"]
+    ],
+    [
+        "titulo" => "Trabalho como artista independente",
+        "datainicio" => "2024-6-10",
+        "relevancia" => "5",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Ilustração digital","Web develop (HTML, CSS, JavaScript)"],
+        "tipo" => ["design","ilustração"]
+    ],
+    [
+        "titulo" => "Redesign e reestruturação do site da Farmacia Luz Marques",
+        "datainicio" => "2025-7-10",
+        "relevancia" => "4",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Paginação"],
+        "tipo" => ["design","webdesign"]
+    ],
+    [
+        "titulo" => "Cartaz para a vigilia pela paz",
+        "datainicio" => "2025-3-12",
+        "datafim" => "2025-3-1",
+        "relevancia" => "1",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Paginação","Ilustração digital","Tipografia"],
+        "tipo" => ["design"]
+    ],
+    [
+        "titulo" => "Cartaz para encontro 25 abril LOC",
+        "datainicio" => "2024-2-25",
+        "datafim" => "2024-3-04",
+        "relevancia" => "1",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Paginação","Ilustração digital","Tipografia"],
+        "tipo" => ["design"]
+    ],
+    [
+        "titulo" => "Desdobravel e cartaz para a Vigilia por um planeta amigo da vida",
+        "datainicio" => "2023-11-14",
+        "datafim" => "2023-11-24",
+        "relevancia" => "2",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["Paginação","Tipografia"],
+        "tipo" => ["design"]
+    ],
+    [
+        "titulo" => "Releitura da obra 'Cafe Terrace at Night' de Van Gogh em pontilhismo, traço e pastel de oleo",
+        "datainicio" => "2022-2-14",
+        "datafim" => "2022-05-10",
+        "relevancia" => "3",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["trabalho artesanal"],
+        "tipo" => ["ilustração"]
+    ],
+    [
+        "titulo" => "Corte e custura de 2 camisas",
+        "datainicio" => "2021-07-14",
+        "datafim" => "2021-07-17",
+        "relevancia" => "2",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => ["trabalho artesanal"],
+        "tipo" => []
+    ],
+    [
+        "titulo" => "Se as luzes apagar, a ver estrelas vou ficar - Fundação Ilidio Pinho",
+        "datainicio" => "2016-10-10",
+        "datafim" => "2021-07-17",
+        "relevancia" => "1",
+        "imagem" => "2.png",
+        "pagina" => "portfolio.html",
+        "competencias" => [],
+        "tipo" => []
+    ]
+];
+?>
